1a923332477c3b016ad99a3a880542478da8f9dd
innomat <info@innomat.ch>
2021-05-02 20:59:08 +0200

    OCI Connection

---
+++ erpnext_oci/erpnext_oci/doctype/oci_connection/oci_connection.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Asprotec AG and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('OCI Connection', {
+	// refresh: function(frm) {
+
+	// }
+});
